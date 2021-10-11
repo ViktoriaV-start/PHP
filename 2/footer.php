4edd86c4d548086c38c91010b65079a3495ef543
ViktoriaV <august.flamenco@gmail.com>
2021-10-11 12:43:41 +0300

    Homework 2

---
+++ 2/footer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lesson 2</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400&display=swap" rel="stylesheet">
+    <link href="style.css?ver=<?php echo time();?>" rel="stylesheet">
+<!--    <link href="style.css" rel="stylesheet">-->
+</head>
+<body>
+<footer class = "footer">
+<?php echo $date ?>
+</footer>
+
+</body>
+</html>
